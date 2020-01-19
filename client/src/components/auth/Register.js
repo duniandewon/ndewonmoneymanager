b6b07e402df06e97e785c319b9c17975875898b4
Mohammad Baragbah <duniandewon@gmail.com>
2020-01-19 15:59:39 +0700

    Add alert component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,13 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
+
+/** Alert context */
+import alertContext from '../../context/alert/alertContext';
 
 /** Bootstrap components */
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const Register = () => {
   const [user, setUser] = useState({
@@ -12,6 +16,8 @@ const Register = () => {
     password: '',
     confPassword: ''
   });
+
+  const { setAlert } = useContext(alertContext);
 
   const { name, email, password, confPassword } = user;
 
@@ -22,6 +28,17 @@ const Register = () => {
     });
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please fill in all fields', 'danger');
+    } else if (password !== confPassword) {
+      setAlert("Passwords don't match", 'danger');
+    } else {
+      console.log(user);
+    }
+  };
+
   return (
     <Fragment>
       <h1 className='text-center'>
@@ -29,7 +46,7 @@ const Register = () => {
       </h1>
       <Row className='justify-content-center'>
         <Col xs={6}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId='name'>
               <Form.Label>Name</Form.Label>
               <Form.Control
@@ -66,6 +83,9 @@ const Register = () => {
                 onChange={handleChange}
               />
             </Form.Group>
+            <Button variant='success' type='submit' size='lg' block>
+              Register
+            </Button>
           </Form>
         </Col>
       </Row>
